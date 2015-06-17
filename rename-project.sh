2931129c26894cf3ab600748965987912dfca00e
joaoraf <joaoraf@gmail.com>
2015-06-17 16:22:49 -0300

    Tutorial

--- rename-project.sh
+++ rename-project.sh
@@ -2,7 +2,7 @@
 if [ "$1" == "" ] ; then
    echo "Run: sh rename-project.sh {new_name}"
 else
-  ATUAL=`grep '^name\s*:=' build.sbt | sed -e 's/.*"\(.*\)"/\1/g'`
+  ATUAL=`grep '^name\s*:=' build.sbt | sed -e 's/.*"\+\([^"]\+\)"\+/\1/g'`
   echo "Renaming from $ATUAL to $1"
   find . -name \*.sbt -or -name \*.scala -or -name \*.html -or -name \*.conf -or -name \*.xml -or -name \*.json -or -name \*.md | grep -v target | xargs -n1 sed -e "s/$ATUAL/$1/g" -i
 fi
